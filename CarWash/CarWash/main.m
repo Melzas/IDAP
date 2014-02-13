75e8ccc8655001be5d33b48a826fb25d27c12058
Anton Rayev <sfdsrc@gmail.com>
2014-02-13 13:46:19 +0200

    Car Wash

---
+++ CarWash/CarWash/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CarWash
+//
+//  Created by Anton on 13.02.14.
+//  Copyright (c) 2014 IDAP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {	    
+	}
+    return 0;
+}
+
