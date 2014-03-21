e28b93eeecdc8da0420ca143d00835cc9be6d61a
Anton Rayev <sfdsrc@gmail.com>
2014-03-21 11:26:00 +0200

    FB Friends: add array of user models

---
+++ FacebookFriends/FacebookFriends/Models/FFUsersData.h
@@ -0,0 +1,24 @@
+//
+//  FFUsersData.h
+//  FacebookFriends
+//
+//  Created by Anton Rayev on 3/20/14.
+//  Copyright (c) 2014 Anton Rayev. All rights reserved.
+//
+
+#import "IDPModel.h"
+
+@class FFUserData;
+
+@interface FFUsersData : IDPModel
+@property (nonatomic, readonly)	NSArray		*users;
+@property (nonatomic, readonly)	NSString	*savePath;
+
+- (void)addUser:(FFUserData *)user;
+- (void)removeUser:(FFUserData *)user;
+
+- (void)moveUserFromIndex:(NSUInteger)fromIndex toIndex:(NSUInteger)toIndex;
+
+- (void)save;
+
+@end
---
+++ FacebookFriends/FacebookFriends/Models/FFUsersData.m
@@ -0,0 +1,77 @@
+//
+//  FFUsersData.m
+//  FacebookFriends
+//
+//  Created by Anton Rayev on 3/20/14.
+//  Copyright (c) 2014 Anton Rayev. All rights reserved.
+//
+
+#import "FFUsersData.h"
+
+#import "FFUsersData.h"
+
+static NSString * const kFFStorageFileName = @"kFFStorageFileName.plist";
+
+@interface FFUsersData ()
+@property (nonatomic, retain)	NSMutableArray	*mutableUsers;
+
+@end
+
+@implementation FFUsersData
+
+@dynamic users;
+@dynamic savePath;
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)cleanup {
+	[self save];
+	
+	self.mutableUsers = nil;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSArray *)users {
+	return [[self.mutableUsers copy] autorelease];
+}
+
+- (NSString *)savePath {
+	NSString *libraryDirectoryPath = [NSFileManager libraryDirectoryPath];
+	NSString *cacheDirectoryPath = [libraryDirectoryPath stringByAppendingPathComponent:@"caches"];
+	return [cacheDirectoryPath stringByAppendingPathComponent:kFFStorageFileName];
+}
+
+#pragma mark -
+#pragma mark Public
+
+- (void)addUser:(FFUserData *)user {
+	[self.mutableUsers addObject:user];
+}
+
+- (void)removeUser:(FFUserData *)user {
+	[self.mutableUsers removeObject:user];
+}
+
+- (void)moveUserFromIndex:(NSUInteger)fromIndex toIndex:(NSUInteger)toIndex {
+	NSMutableArray *users = self.mutableUsers;
+	
+	FFUserData *userToMove = [[[users objectAtIndex:fromIndex] retain] autorelease];
+	[users removeObjectAtIndex:fromIndex];
+	[users insertObject:userToMove atIndex:toIndex];
+}
+
+#pragma mark -
+#pragma mark Private
+
+- (void)prepareForLoad {
+	self.mutableUsers = [NSMutableArray array];
+}
+
+- (void)save {
+	[NSKeyedArchiver archiveRootObject:self.mutableUsers toFile:self.savePath];
+}
+
+@end
